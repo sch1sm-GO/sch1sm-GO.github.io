9c8e98f02fd819387446ce78328b6dc1a2310022
sch1sm-GO <101189861+sch1sm-GO@users.noreply.github.com>
2022-03-11 18:17:22 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
 let targetImage = document.querySelector("#smart-image");
-targetImage.classList.remove("small");
+targetImage.addEventListener("click", function() {
+    targetImage.classList.remove("small");
+});
 });
 
